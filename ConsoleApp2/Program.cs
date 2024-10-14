af3621ef48d0f7c4ad4776b987ad270a6d02c419
k0neba <165696402+k0neba@users.noreply.github.com>
2024-10-14 15:47:45 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int N = 20; // Наприклад, створюємо колекцію з 20 чисел
+
+        // Створення колекції випадкових чисел в інтервалі [1; 20]
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < N; i++)
+        {
+            numbers.Add(rand.Next(1, 21));
+        }
+
+        Console.WriteLine("Початкова колекція:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        // Розділення на парні та непарні числа
+        List<int> evenNumbers = new List<int>();
+        List<int> oddNumbers = new List<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenNumbers.Add(number);
+            }
+            else
+            {
+                oddNumbers.Add(number);
+            }
+        }
+
+        Console.WriteLine("Колекція парних чисел:");
+        foreach (int number in evenNumbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Колекція непарних чисел:");
+        foreach (int number in oddNumbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int N = 20; // Наприклад, створюємо колекцію з 20 чисел
+
+        // Створення колекції випадкових чисел в інтервалі [5; 25]
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < N; i++)
+        {
+            numbers.Add(rand.Next(5, 26));
+        }
+
+        Console.WriteLine("Початкова колекція:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        // Пошук та видалення останнього числа, яке закінчується на 1
+        for (int i = numbers.Count - 1; i >= 0; i--)
+        {
+            if (numbers[i] % 10 == 1)
+            {
+                numbers.RemoveAt(i);
+                break;
+            }
+        }
+
+        Console.WriteLine("Колекція після видалення останнього числа, яке закінчується на 1:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+}
